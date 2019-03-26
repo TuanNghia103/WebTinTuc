cf3a919c740dd566f9227fd9cdb06fbf434dd0c1
TuanNghia103 <48285347+TuanNghia103@users.noreply.github.com>
2019-03-26 21:34:29 +0700

    26032019

---
+++ WebsiteTinTuc2/WebTinTuc.Data/Entities/DefaultDBContext.cs
@@ -0,0 +1,33 @@
+namespace WebTinTuc.Data.Entities
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class DefaultDBContext : DbContext
+    {
+        public DefaultDBContext()
+            : base("name=DefaultDBContext")
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Function> Functions { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<PostComment> PostComments { get; set; }
+        public virtual DbSet<PostRate> PostRates { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<RoleFunctionRelationship> RoleFunctionRelationships { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserRoleRelationship> UserRoleRelationships { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(e => e.LastName)
+                .IsFixedLength();
+        }
+    }
+}
--- WebsiteTinTuc2/WebsiteTinTuc2/App_Start/RouteConfig.cs
+++ WebsiteTinTuc2/WebsiteTinTuc2/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace WebsiteTinTuc2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WebsiteTinTuc2.Controllers" }
             );
         }
     }
